98cb5842e57ab174032c04fedb3204ed5ed829a0
Arilonchik <egorreal2011@gmail.com>
2020-03-05 19:25:50 +0300

    Changed init

--- web/init.sh
+++ web/init.sh
@@ -1,5 +1,10 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo apt update
+sudo apt install python3.5
+sudo rm /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install django==2.1
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE 'qa' DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci"
 mysql -uroot -e "CREATE USER 'ari'@'localhost' IDENTIFIED BY 'password'"
